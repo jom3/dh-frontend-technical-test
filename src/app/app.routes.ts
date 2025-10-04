37f61d47b2cad6ff3f849e967cb9e474ac3e39c2
joams mogro gomez <joamsmg@gmail.com>
2025-10-04 19:59:46 -0400

    feat: new auth layout was added into the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeLayout } from './layouts/home-layout/home-layout';
+import { AuthLayout } from './layouts/auth-layout/auth-layout';
 
 export const routes: Routes = [
   {
     path: '',
+    component: AuthLayout,
     loadChildren: () => import('./features/auth/routes/auth.routes'),
   },
   {
-    path:'',
-    component:HomeLayout,
-    children:[
+    path: '',
+    component: HomeLayout,
+    children: [
       //aqui sus hijos
     ]
   }
