0fb33c81d6b3bc3c9c1ae4332e2851d1ca993118
joams mogro gomez <joamsmg@gmail.com>
2025-10-04 19:57:57 -0400

    feat: brand logo done

---
+++ src/app/shared/components/brand-logo/brand-logo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'brand-logo',
+  imports: [],
+  templateUrl: './brand-logo.html',
+  styleUrl: './brand-logo.css'
+})
+export class BrandLogo {
+
+}
