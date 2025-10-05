3c185df87f28d08a0b66554c83a29c8f66f0dd89
joams mogro gomez <joamsmg@gmail.com>
2025-10-04 20:03:26 -0400

    feat: some imports were moved to app.config and a new font was added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,9 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
+import '@fontsource/pacifico';
+import '@fontsource-variable/inter';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
