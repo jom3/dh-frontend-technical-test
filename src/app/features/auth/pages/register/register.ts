ed548418bb35ea7cd24119eae69d1ad8b53ed002
joams mogro gomez <joamsmg@gmail.com>
2025-10-05 11:41:10 -0400

    feat: register functionality end until changes

--- src/app/features/auth/pages/register/register.ts
+++ src/app/features/auth/pages/register/register.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BrandLogo } from "../../../../shared/components/brand-logo/brand-logo";
 import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RegisterUser } from '../../service/register-user';
 
 @Component({
   selector: 'app-register',
-  imports: [BrandLogo, RouterLink],
+  imports: [BrandLogo, RouterLink, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export default class Register {
 
+  private readonly fb = inject(FormBuilder);
+  private readonly registerUserSvc = inject(RegisterUser)
+
+  registerForm = this.fb.group({
+    email: this.fb.nonNullable.control('', [Validators.email, Validators.required]),
+    password: this.fb.nonNullable.control('', [Validators.required, Validators.minLength(8)])
+  });
+
+  onRegisterSubmit() {
+    if (!this.registerForm.valid) {
+      this.registerForm.markAllAsTouched()
+      return
+    }
+    const { email, password } = this.registerForm.getRawValue()
+    console.log(this.registerUserSvc.registerUser({email, password}));
+  }
+
+  hasError(controlName: string, error: string): boolean {
+    const control = this.registerForm.get(controlName);
+    return !!(control && control.hasError(error) && (control.touched || control.dirty));
+  }
 }
