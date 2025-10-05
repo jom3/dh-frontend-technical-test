f8133819834cc16a55ab051a1b688aa7afd56d99
joams mogro gomez <joamsmg@gmail.com>
2025-10-04 23:55:59 -0400

    feat: partial updated for register component

--- src/app/features/auth/pages/register/register.ts
+++ src/app/features/auth/pages/register/register.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { BrandLogo } from "../../../../shared/components/brand-logo/brand-logo";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [BrandLogo, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
